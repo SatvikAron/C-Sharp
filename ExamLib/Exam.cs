b8a0c0264faa9d08a2cbfda0dce3d0b2e7fe3dfb
Jahurul Islam <jahurul1983@gmail.com>
2017-09-20 10:48:09 +0200

    Examtest

---
+++ ExamLib/Exam.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamLib
+{
+    public class Exam
+    {
+       public string CourseName;
+        private Dictionary<Student, string> Studentdictionary = new Dictionary<Student, string>();
+        List<Student> StudentsList = new List<Student>();
+
+        public Exam(string coursename)
+        {
+            this.CourseName = coursename;
+        }
+
+        public void Assign(Student student)
+        {
+            StudentsList.Add(student);
+            Studentdictionary.Add(student,"Ig");
+
+
+        }
+      
+
+        public void Grade(Student student, string betyg)
+        {
+            Studentdictionary[student] = betyg;
+        }
+
+        public Dictionary<string, int> GenerateStatistics()
+        {
+            Dictionary<string, int> Statistik = new Dictionary<string, int>();
+            Statistik.Add("IG", 0);
+            Statistik.Add("G", 0);
+            Statistik.Add("VG", 0);
+            foreach (var item in Studentdictionary)
+            {
+                Statistik[item.Value]++;
+            }
+            return Statistik;
+        }
+    }
+}
---
+++ ExamLibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExamLibTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExamLibTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1d974f7-4cc3-48ab-8bd4-41fb8f1b5915")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
